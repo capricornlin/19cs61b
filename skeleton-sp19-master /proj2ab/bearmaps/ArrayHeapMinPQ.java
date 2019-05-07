f9b3aef2a131b488f98c3f0c5e8594eae5e8593a
Caseylin <capricornlin1989@gmail.com>
2019-05-07 14:23:52 +0800

    pA

--- skeleton-sp19-master /proj2ab/bearmaps/ArrayHeapMinPQ.java
+++ skeleton-sp19-master /proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -1,6 +1,6 @@
 package bearmaps;
 
-public class ArrayHeapMinPQ<T extends Comparable<T>> implements ExtrinsicMinPQ<T>{
+public class ArrayHeapMinPQ<T extends Comparable<T>> {
 
     private Node[] pq;        //the class that include <T> and Priority.
     private int n;              //the number of item in pq.
@@ -88,6 +88,27 @@ public class ArrayHeapMinPQ<T extends Comparable<T>> implements ExtrinsicMinPQ<T
             return x.priority;
     }
 
+    public void changeP(T key,double p){
+        changeP(root, key,p);
+    }
+
+    private void changeP(ItemNode x,T key,double p){
+        if(key == null)
+            return ;
+        if(x == null)
+            return ;
+        int cmp = key.compareTo(x.item);
+        if(cmp < 0)
+            changeP(x.left,key,p);
+        else if(cmp > 0)
+            changeP(x.right,key,p);
+        else
+            x.priority = p;
+    }
+
+
+
+
     /** ===================  ItemNode  ============================ */
 
     /**T his is the Node that include <T> items and Priority number. */
@@ -123,7 +144,6 @@ public class ArrayHeapMinPQ<T extends Comparable<T>> implements ExtrinsicMinPQ<T
     /* Adds an item with the given priority value. Throws an
      * IllegalArgumentExceptionb if item is already present.
      * You may assume that item is never null. */
-    @Override
     public void add(T item, double priority){
         /** add item and priority to the ItemNode */
         put(item,priority);
@@ -140,7 +160,6 @@ public class ArrayHeapMinPQ<T extends Comparable<T>> implements ExtrinsicMinPQ<T
     }
 
     /* Returns true if the PQ contains the given item. */
-    @Override
     public boolean contains(T item){
         double s = get(item);
         if(s == 99.9999)
@@ -150,7 +169,6 @@ public class ArrayHeapMinPQ<T extends Comparable<T>> implements ExtrinsicMinPQ<T
     }
 
     /* Returns the minimum item. Throws NoSuchElementException if the PQ is empty. */
-    @Override
     public T getSmallest(){
         /** (T)Object!? */
         T x = (T)pq[1].getItems();
@@ -161,7 +179,6 @@ public class ArrayHeapMinPQ<T extends Comparable<T>> implements ExtrinsicMinPQ<T
 
 
     /* Removes and returns the minimum item. Throws NoSuchElementException if the PQ is empty. */
-    @Override
     public T removeSmallest(){
         swap(1,n-1);
         Node s = pq[n-1];
@@ -174,17 +191,167 @@ public class ArrayHeapMinPQ<T extends Comparable<T>> implements ExtrinsicMinPQ<T
 
 
     /* Returns the number of items in the PQ. */
-    @Override
     public int size(){
         return n-1;
     }
 
     /* Changes the priority of the given item. Throws NoSuchElementException if the item
      * doesn't exist. */
-    @Override
     public void changePriority(T item, double priority){
+        double s = get(item);
+        changeP(item,priority);
+        Node x = searchP(s,1);
+        int n  = searchNum(s,1);
+        if(x == null){
+            return;
+        }else{
+            x.priority = priority;
+
+            if(parent(n) == 0){
+                swimdown(n);
+            }
+            else if (pq[n].priority < pq[parent(n)].priority){
+                swim(n);
+            }
+
+            else if(rightchild(n) >size() && leftchild(n) > size()){
+                return;
+            }
+            else if(pq[n].priority > pq[leftchild(n)].priority || pq[n].priority > pq[rightchild(n)].priority){
+                swimdown(n);
+            }else
+                return;
+        }
+
+    }
+
+    /** Search which index of the double a in pq[] Array */
+    private int searchNum(double a,int n){
+        Node x;
+        if(a == pq[n].priority) {
+            //return (T)pq[1].items;
+            x = pq[n];
+            return n;
+        }
+        /** if n is greater than size */
+        if(rightchild(n) > size() && leftchild(n) > size())
+            return 0;
+        else if(rightchild(n) > size()){
+            n = leftchild(n);
+            return searchNum(a,n);
+        }else if(leftchild(n) > size()){
+            n = rightchild(n);
+            return searchNum(a,n);
+        }
 
 
+
+
+        /** if n is not greater than size */
+        else if( a == pq[leftchild(n)].priority){
+            x = pq[leftchild(n)];
+            return leftchild(n);
+        }else if(a == pq[rightchild(n)].priority){
+            x = pq[rightchild(n)];
+            return rightchild(n);
+        }
+
+        /** go left */
+        else if(a > pq[leftchild(n)].priority && a < pq[rightchild(n)].priority){
+            {
+                n = leftchild(n);
+                return searchNum(a,n);
+            }
+
+        }
+        /** go right */
+        else if(a > pq[rightchild(n)].priority && a < pq[leftchild(n)].priority){
+            {
+                n = rightchild(n);
+                return searchNum(a,n);
+            }
+        }
+        /** same priority */
+        else if(a > pq[rightchild(n)].priority){
+            if(a > pq[leftchild(n)].priority){
+                int left = leftchild(n);
+                int right = rightchild(n);
+                int l = searchNum(a,left);
+                int r = searchNum(a,right);
+                if(l != 0)
+                    return l;
+                else if(r != 0)
+                    return r;
+
+            }
+        }
+
+        return 99999999;
+    }
+
+
+
+    /** Search which Node of the double a in pq[] Array */
+    private Node searchP(double a,int n){
+        Node x;
+
+        if(a == pq[n].priority) {
+            //return (T)pq[1].items;
+            x = pq[n];
+            return x;
+        }
+        /** if n is greater than size */
+        if(rightchild(n) > size() && leftchild(n) > size())
+            return null;
+        else if(rightchild(n) > size()){
+            n = leftchild(n);
+            return searchP(a,n);
+        }else if(leftchild(n) > size()){
+            n = rightchild(n);
+            return searchP(a,n);
+        }
+
+
+        /** if n is not greater than size */
+        else if( a == pq[leftchild(n)].priority){
+            x = pq[leftchild(n)];
+            return x;
+        }else if(a == pq[rightchild(n)].priority){
+            x = pq[rightchild(n)];
+            return x;
+        }
+
+        /** go left */
+        else if(a > pq[leftchild(n)].priority && a < pq[rightchild(n)].priority){
+            {
+                n = leftchild(n);
+                return searchP(a,n);
+            }
+
+        }
+        /** go right */
+        else if(a > pq[rightchild(n)].priority && a < pq[leftchild(n)].priority){
+            {
+                n = rightchild(n);
+                return searchP(a,n);
+            }
+        }
+        /** same priority */
+        else if(a > pq[rightchild(n)].priority && a > pq[leftchild(n)].priority){
+            {
+                int left = leftchild(n);
+                int right = rightchild(n);
+                //search both side right and left.
+                Node l = searchP(a,left);
+                Node r = searchP(a,right);
+                if(l != null)
+                    return l;
+                else if(r != null)
+                    return r;
+            }
+        }
+
+        return null;
     }
 
 
@@ -199,6 +366,9 @@ public class ArrayHeapMinPQ<T extends Comparable<T>> implements ExtrinsicMinPQ<T
 
     /** the parents of the Node */
     private int parent(int k){
+        //k is the root.
+        if(k == 1)
+            return 0;
         return k/2;
     }
 
@@ -224,18 +394,27 @@ public class ArrayHeapMinPQ<T extends Comparable<T>> implements ExtrinsicMinPQ<T
         }
     }
 
+    /** If index k's item greater to their children, we need to swap with their children. */
     private void swimdown(int k){
         /**left child smaller than right child */
-        if(pq[rightchild(k)]  == null)
+        if(rightchild(k) > size() && leftchild(k) > size())
             return;
-        if(pq[leftchild(k)] == null)
+        if(rightchild(k)  > size() && pq[leftchild(k)].priority < pq[k].priority){
+            swap(k,leftchild(k));
             return;
+        }
+        if(leftchild(k) >size() && pq[rightchild(k)].priority < pq[k].priority){
+            swap(k,rightchild(k));
+            return;
+        }
+        //go left.
         if(pq[leftchild(k)].getPriority() < pq[rightchild(k)].getPriority()){
             if(pq[k].getPriority() > pq[leftchild(k)].getPriority()){
                 swap(k,leftchild(k));
                 k = leftchild(k);
                 swimdown(k);
             }
+            //go right.
         }else{
             if(pq[k].getPriority() > pq[rightchild(k)].getPriority()){
                 swap(k,rightchild(k));
@@ -266,13 +445,14 @@ public class ArrayHeapMinPQ<T extends Comparable<T>> implements ExtrinsicMinPQ<T
         a.add("aloha",4.9);
         a.add("hawaii",9.9);
         a.add("good morning",4.2);
-        a.add("LOLLL",0.6);
+        a.add("LOLLL",0.2);
+        a.changePriority("LOLLL",8);
         //a.add("kolll",10.2);
         //a.removeSmallest();
         //a.removeSmallest();
         //System.out.println(a.size());
         //System.out.println(a.getSmallest());
-        System.out.println(a.contains("good mornin"));
+        System.out.println(a.contains("hello"));
 
     }
 
